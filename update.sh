b4a06177ba3284db3068e403eeb0d614d2260828
Robbie Clutton <robert.clutton@gmail.com>
2013-09-02 15:42:59 -0400

    update script for directories

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ 
+for DIR in $(find ./repo-* -type d  \( ! -regex '.*/\..*' \) ); do
+  echo -e "Creating directory listing index.html for "$DIR
+  (
+    echo  "<html><body><h1>Directory listing</h1><hr/><pre>"
+    ls -1pa "${DIR}" |\
+      grep -v "^\./$" |\
+      grep -v "^\..*/$" |\
+      grep -v "^index\.html$"|\
+      awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+      echo -e "</pre></body></html>"
+  ) > "${DIR}/index.html"
+done
